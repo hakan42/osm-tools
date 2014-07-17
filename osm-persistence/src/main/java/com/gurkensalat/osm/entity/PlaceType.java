35a8b4dad062125c51b70356f4783e63737d14a3
Hakan Tandogan <hakan@gurkensalat.com>
2014-07-17 23:38:55 +0200

    Place of Worship in preparation for osm-mosques project

--- osm-persistence/src/main/java/com/gurkensalat/osm/entity/PlaceType.java
+++ osm-persistence/src/main/java/com/gurkensalat/osm/entity/PlaceType.java
@@ -3,6 +3,7 @@ package com.gurkensalat.osm.entity;
 public enum PlaceType
 {
     OSM_CITY("OSM_CITY"),
+    OSM_PLACE_OF_WORSHIP("OSM_PLACE_OF_WORSHIP"),
     IL_ILCE_DUMP_CITY("IL_ILCE_DUMP_CITY");
 
     private final String name;
