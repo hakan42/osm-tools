aa3981dbb2f9f11a22786d77bbcf03335a46ae2a
Hakan Tandogan <hakan@gurkensalat.com>
2014-08-17 22:48:50 +0200

    Use MappedSuperClass for common data

--- osm-persistence/src/main/java/com/gurkensalat/osm/entity/OsmPlace.java
+++ osm-persistence/src/main/java/com/gurkensalat/osm/entity/OsmPlace.java
@@ -1,186 +1,23 @@
 package com.gurkensalat.osm.entity;
 
-import org.springframework.data.jpa.domain.AbstractPersistable;
-
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.persistence.Table;
-import javax.persistence.Version;
 
 @Entity
 @Table(name = "OSM_PLACES")
-public class OsmPlace extends AbstractPersistable<Long>
+public class OsmPlace extends OsmPlaceBase
 {
-    @Version
-    @Column(name = "VERSION")
-    private Integer version;
-
-    @Column(name = "VALID")
-    private boolean valid;
-
-    @Column(name = "TYPE", length = 20)
-    @Enumerated(EnumType.STRING)
-    private PlaceType type;
-
-    @Column(name = "D_KEY", length = 80)
-    private String key;
-
-    @Column(name = "LAT")
-    private double lat;
-
-    @Column(name = "LON")
-    private double lon;
-
-    @Column(name = "NAME", length = 80)
-    private String name;
-
-    private Address address;
-
     protected OsmPlace()
     {
     }
 
     public OsmPlace(String name, PlaceType type)
     {
-        this.name = name;
-        this.type = type;
+        super(name, type);
     }
 
     public OsmPlace(OsmNode node)
     {
-        this.setLat(node.getLat());
-        this.setLon(node.getLon());
-        this.setAddress(new Address());
-
-        for (OsmTag tag : node.getTags())
-        {
-            String key = tag.getKey().toLowerCase();
-            String val = tag.getValue();
-
-            if ("name".equals(key))
-            {
-                this.setName(val);
-            }
-            else if ("addr:street".equals(key))
-            {
-                this.getAddress().setStreet(val);
-            }
-            else if ("addr:housenumber".equals(key))
-            {
-                this.getAddress().setHousenumber(val);
-            }
-            else if ("addr:postcode".equals(key))
-            {
-                this.getAddress().setPostcode(val);
-            }
-            else if ("addr:city".equals(key))
-            {
-                this.getAddress().setCity(val);
-            }
-        }
-    }
-
-    public void copyTo(OsmPlace other)
-    {
-        other.setLon(getLon());
-        other.setLat(getLat());
-        other.setName(getName());
-        other.setType(getType());
-        other.setKey(getKey());
-        other.setValid(isValid());
-
-        if (getAddress() == null)
-        {
-            setAddress(new Address());
-        }
-
-        if (other.getAddress() == null)
-        {
-            other.setAddress(new Address());
-        }
-
-        getAddress().copyTo(other.getAddress());
-    }
-
-    public boolean isValid()
-    {
-        return valid;
-    }
-
-    public void setValid(boolean valid)
-    {
-        this.valid = valid;
-    }
-
-    public Address getAddress()
-    {
-        return address;
-    }
-
-    public void setAddress(Address address)
-    {
-        this.address = address;
-    }
-
-    public double getLat()
-    {
-        return lat;
-    }
-
-    public void setLat(double lat)
-    {
-        this.lat = lat;
-    }
-
-    public double getLon()
-    {
-        return lon;
-    }
-
-    public void setLon(double lon)
-    {
-        this.lon = lon;
-    }
-
-    public String getKey()
-    {
-        return key;
-    }
-
-    public void setKey(String key)
-    {
-        this.key = key;
-    }
-
-    public String getName()
-    {
-        return name;
-    }
-
-    public void setName(String name)
-    {
-        this.name = name;
-    }
-
-    public PlaceType getType()
-    {
-        return type;
-    }
-
-    public void setType(PlaceType type)
-    {
-        this.type = type;
-    }
-
-    public Integer getVersion()
-    {
-        return version;
-    }
-
-    public void setVersion(Integer version)
-    {
-        this.version = version;
+        super(node);
     }
 }
