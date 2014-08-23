ff593ce3856a866e9474dedb560e6b56d57c058e
Hakan Tandogan <hakan@gurkensalat.com>
2014-08-23 23:29:30 +0200

    Some handy, if not correct, constants

---
+++ osm-utils/src/main/java/com/gurkensalat/osm/utils/DistanceConstants.java
@@ -0,0 +1,10 @@
+package com.gurkensalat.osm.utils;
+
+public class DistanceConstants
+{
+    public static final double DELTA_LAT_LON_100_M = 0.001;
+
+    private static final double DELTA_LAT_LON_1_KM = 0.01;
+
+    private static final double DELTA_LAT_LON_10_KM = 0.1;
+}
