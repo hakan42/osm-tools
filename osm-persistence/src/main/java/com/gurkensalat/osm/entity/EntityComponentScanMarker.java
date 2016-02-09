d575fcdb6f81cc7836e6ad8f3a9a30e14f3eae75
Hakan Tandogan <hakan@tandogan.com>
2016-02-09 18:03:11 +0100

    Adapt to JDK 1.8 Javadoc

--- osm-persistence/src/main/java/com/gurkensalat/osm/entity/EntityComponentScanMarker.java
+++ osm-persistence/src/main/java/com/gurkensalat/osm/entity/EntityComponentScanMarker.java
@@ -2,7 +2,6 @@ package com.gurkensalat.osm.entity;
 
 /**
  * Empty marker interface to make this package visible to EnableJpaRepositories.
- * <p/>
  *
  * @see <a href="http://stackoverflow.com/questions/23366226/spring-boot-w-jpa-move-entity-to-different-package">StackOverflow explanation</a>
  */
